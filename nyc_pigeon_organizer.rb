cbc5216c14a2530aee084429188130bff5b42b67
PoloPro <wesleybdavis@gmail.com>
2016-03-28 02:51:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+
+  organized_data = {}
+
+  pigeon_data.each do |field, option_hash|
+    option_hash.each do |option, name_array|
+      name_array.each do |name|
+        if organized_data.has_key?(name)
+          if organized_data[name].has_key?(field)
+            organized_data[name][field] << option.to_s
+          else
+            organized_data[name][field] = [option.to_s]
+          end
+        else
+          organized_data[name] = {field => [option.to_s]}
+        end
+      end
+    end
+  end
+
+  organized_data
+
 end
